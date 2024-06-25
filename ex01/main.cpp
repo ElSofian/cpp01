11ef7b0b29161c5019100ea73ba9eadf285c6df5
ElSofian <sofian.developpeur@gmail.com>
2024-06-25 11:06:20 +0200

    improve main (ex01)

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,15 +1,13 @@
 #include "Zombie.hpp"
 
-void delete_horde(Zombie *horde)
-{
-    delete [] horde;
-}
-
 int main(void)
 {
     Zombie *horde = zombieHorde(10, "Soso");
     for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << ". ";
         horde[i].announce();
-    delete_horde(horde);
+    }
+    delete [] horde;
     return 0;
 }
